5d4d28b4ec09eac00e9ae8c6d4cb35e62b394b1d
Andres Plana <dudeandu@hotmail.com>
2021-09-14 13:16:51 -0400

    update for today's date

--- COVID-cases-ontario-schools.R
+++ COVID-cases-ontario-schools.R
@@ -19,11 +19,65 @@ lastDate <- data_active_cases_raw[nrow(data_active_cases_raw),"collected_date"]
 ## GET all instances whit the matching last date
 lastdayinstances <- data_active_cases_raw[ which(data_active_cases_raw$collected_date == lastDate), ] 
 
+# get the school list 
+school_number_list <- unique(data_allSchools_raw$School.Number)
+
+# make all school numbers/id numeric
+lastdayinstances$school_id <- as.numeric(lastdayinstances$school_id)
+data_allSchools_raw$School.Number <- as.numeric(data_allSchools_raw$School.Number)
+
+## build a data frame with schools info and cases
+all_schools_cases_df <- data_allSchools_raw
+
+# Add the columns from the active cases data frame
+all_schools_cases_df$collected_date <- rep(FALSE,times = nrow(all_schools_cases_df))
+all_schools_cases_df$reported_date <- rep(FALSE,times = nrow(all_schools_cases_df))
+all_schools_cases_df$confirmed_student_cases <- rep(0,times = nrow(all_schools_cases_df))
+all_schools_cases_df$confirmed_staff_cases <- rep(0,times = nrow(all_schools_cases_df))
+all_schools_cases_df$confirmed_unspecified_cases <- rep(0,times = nrow(all_schools_cases_df))
+all_schools_cases_df$total_confirmed_cases <- rep(0,times = nrow(all_schools_cases_df))
+
+
+## search the active cases database, and change the number of cases based on a school id search
+## some school boards are in there, so have to make an if statement looking for NAs in school id, and then match to the school board name
+for(i in 1:nrow(lastdayinstances)) {
+  
+  # print(i)
+  # get the schoolid
+  temp_schoolid <- lastdayinstances[i,"school_id"]
+  print(temp_schoolid)
+  
+  if ( !is.na(temp_schoolid) ) {
+    print(all_schools_cases_df[ which(all_schools_cases_df$School.Number == temp_schoolid), "School.Number" ])
+    
+      all_schools_cases_df[ which(all_schools_cases_df$School.Number == temp_schoolid), "collected_date"] <- lastdayinstances[i,"collected_date"]
+      all_schools_cases_df[ which(all_schools_cases_df$School.Number == temp_schoolid), "reported_date"] <- lastdayinstances[i,"reported_date"]
+      all_schools_cases_df[ which(all_schools_cases_df$School.Number == temp_schoolid), "confirmed_student_cases"] <- lastdayinstances[i,"confirmed_student_cases"]
+      all_schools_cases_df[ which(all_schools_cases_df$School.Number == temp_schoolid), "confirmed_staff_cases"] <- lastdayinstances[i,"confirmed_staff_cases"]
+      all_schools_cases_df[ which(all_schools_cases_df$School.Number == temp_schoolid), "confirmed_unspecified_cases"] <- lastdayinstances[i,"confirmed_unspecified_cases"]
+      all_schools_cases_df[ which(all_schools_cases_df$School.Number == temp_schoolid), "total_confirmed_cases"] <- lastdayinstances[i,"total_confirmed_cases"]
+  }  
+  
+  # new_temp_data <- data_edges_raw[ which(data_edges_raw$timezone == timezones_list[i]), ]
+  # temp_freq_usernames <- count(new_temp_data, 'username')
+  
+  # timezonerow <- rep(timezones_list[i],times = nrow(temp_freq_usernames))
+  # temp_freq_usernames_daily <- cbind(temp_freq_usernames,timezonerow)
+  # 
+  # edges_aggregated_byusername <- rbind(edges_aggregated_byusername,temp_freq_usernames_daily)
+}
+
+
+
 # add an id number to all instances of the last day
-lastdayinstances$id <- seq.int(0,nrow(lastdayinstances)-1)
+# !!!!!!!! might be better to do this to the larger data frame at the end
+all_schools_cases_df$id <- seq.int(0,nrow(all_schools_cases_df)-1)
+
+colnames(all_schools_cases_df) <- c("Region","Board_Number","Board_Name","Board_Type","Board_Language","School_Number","School_Name","School_Level","School_Language","School_Type","School_Special_Conditions_Code","Suite","PO_Box","Street","City","Province","Postal_Code","Phone","Fax","Grade_Range","Date_Open","School_Email","School_Website","Board_Website","collected_date","reported_date","confirmed_student_cases","confirmed_staff_cases","confirmed_unspecified_cases","total_confirmed_cases","id")
+
 
 ## svae the file into a new CSV
-write.csv(lastdayinstances,"schoolsactivecovid_adaiID.csv", row.names = FALSE)
+write.csv(all_schools_cases_df,"schoolsactivecovid_adaiID.csv", row.names = FALSE)
 
 
 
